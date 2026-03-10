c6a03da0dc1c4817247a4cdcd2184cd775fb2d47
Rodrigo Estape de Oliveira <rodrigo_estape@hotmail.com>
2026-03-10 17:20:41 -0300

    Update: Reverting the target platform back to Windows.

--- source/PS1_Remake/Engine/include/SetAttr.h
+++
@@ -1,9 +0,0 @@
-#include <SDL3/SDL.h>
-
-class SetAttr
-{
-    public:
-        int InitSDL3();
-        static void SetOpenGL3_3();
-        SDL_Gamepad* GetGamepads(SDL_JoystickID* SDL_Joystick, int num);
-};
